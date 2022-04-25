bae09902d477ce119fc8b0849afd30c3502ad99f
satya <iamsatyaranjan7@gmail.com>
2022-04-25 21:12:24 +0400

    first commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import "antd/dist/antd.css";
+import store from "./app/store";
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+  <Router>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </Router>,
+  document.getElementById("root")
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// dependancies and libraries
+
+// antd, @ant-design/icons, react-redux, html-react-parser, @reduxjs/toolkit
+// axios, moment, millify, react-chartjs-2, chart.js, react-router-dom
+
+
+// IMPORTANT 
+
+// -- By putting the app inside the provider, every single component in the app will have access to the store 
